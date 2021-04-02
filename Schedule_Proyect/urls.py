94db8ce300d49643656bb9509c7acdeb2eb83790
nachovidondo <nachovidondo@gmail.com>
2021-04-02 10:58:54 -0300

    print

--- Schedule_Proyect/urls.py
+++ Schedule_Proyect/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('login/',LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/',LogoutView.as_view(template_name='login.html'), name='logout'),
 ]
-
+print("Nico")
