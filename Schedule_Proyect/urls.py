1db5ad166d565c434aa5c109bb906076d9510ca9
nachovidondo <nachovidondo@gmail.com>
2021-04-03 18:55:05 -0300

    Routers

--- Schedule_Proyect/urls.py
+++ Schedule_Proyect/urls.py
@@ -5,24 +5,11 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Documentacion Personas",
-      default_version='v 0.1',
-      description="Documentacion publica de Profesionales",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('apps.Tags.api.routers')),
-    re_path('swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', include('main.urls')),
+    path('tags/', include('apps.Tags.api.routers')),
+    path('users/', include('apps.Users.api.routers')),
+    path('login/',LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/',LogoutView.as_view(template_name='login.html'), name='logout'),
 ]
