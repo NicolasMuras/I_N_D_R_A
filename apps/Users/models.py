642c9cfae7c043c212f66dfbb60661640ed60544
nachovidondo <nachovidondo@gmail.com>
2021-04-03 18:22:46 -0300

    Correct user model

--- apps/Users/models.py
+++ apps/Users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from apps.Tags.models import Tag
 
-# Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(
+        max_length=200, verbose_name="Nombre", blank=True, null=True
+        )
+    passsword = models.CharField(
+        max_length=200, verbose_name="Contraseña", blank=True, null=True
+        )
+    tags = models.ForeignKey(Tag, on_delete=models.CASCADE, blank=True, null=True)
+    
+    class Meta:
+        verbose_name = "Users"
+        
+    def __str__(self):
+        return self.name
