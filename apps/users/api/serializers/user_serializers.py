a0838191acb0c3b5f6353bf9a53a3b45a496f6f8
nachovidondo <nachovidondo@gmail.com>
2021-04-05 21:42:45 -0300

    Validations and tags manytomany

--- apps/users/api/serializers/user_serializers.py
+++ apps/users/api/serializers/user_serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from apps.users.models import User
+from apps.Users.models import User
 
 
 
 class UserSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
+   class Meta:
+      model = User
+      fields = ('__all__')
--- apps/users/api/views/views_user.py
+++ apps/users/api/views/views_user.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets
 from apps.users.api.serializers.user_serializers import UserSerializers
+from apps.Users.models import User
 
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializers
-    queryset = UserSerializers.Meta.model.objects.all()
+    queryset = User.objects.all()
